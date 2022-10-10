502684026201de0d0f60f47c26906cd55737258b
michals96 <michal.stefaniuk9@gmail.com>
2022-10-10 22:39:56 +0200

    Small refactor

--- backend/src/main/java/App.java
+++ backend/src/main/java/App.java
@@ -6,7 +6,10 @@ public class App {
 
   public static void main(String[] args) throws IOException {
     final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\1-simple-process.json";
-    final var saveFilePath = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\1-simple-process-parsed.json";
+    //final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\2-multiple-human-services.json";
+    //final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\3-branching-process.json";
+    //final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\4-recursive-branching-process.json";
+    final var saveFilePath = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\parsed.json";
 
     loaderService.execute(pathToFile, saveFilePath);
   }
