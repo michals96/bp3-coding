41a1255918b82dd992009a92d58b2f4966a2f738
michals96 <michal.stefaniuk9@gmail.com>
2022-10-11 02:37:29 +0200

    DONE?

--- backend/src/main/java/App.java
+++ backend/src/main/java/App.java
@@ -9,19 +9,13 @@ public class App {
   private static final List<String> FILES =
       List.of("1-simple-process.json", "2-multiple-human-services.json", "3-branching-process.json", "4-recursive-branching-process.json");
 
-  public static void main(String[] args) throws IOException {
-    final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\1-simple-process.json";
-    //final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\2-multiple-human-services.json";
-    //final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\3-branching-process.json";
-    //final var pathToFile = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\4-recursive-branching-process.json";
-    final var saveFilePath = "C:\\Users\\micha\\Desktop\\mr\\bp3-coding\\data\\parsed.json";
-    processService.reduce(pathToFile, saveFilePath);
-/*    FILES.forEach(file -> {
+  public static void main(String[] args) {
+    FILES.forEach(file -> {
       try {
         processService.reduce(FILE_DIR.concat(file), SAVE_FILE_DIR.concat(file));
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
-    });*/
+    });
   }
 }
--- backend/src/main/java/com/bp3/model/SimpleNodeSerializer.java
+++ backend/src/main/java/com/bp3/model/SimpleNodeSerializer.java
@@ -1,6 +1,5 @@
 package com.bp3.model;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
