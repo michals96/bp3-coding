df3348ac80aa877df42115dd0739f5d612123451
michals96 <michal.stefaniuk9@gmail.com>
2022-10-12 21:10:55 +0200

    Init first component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileuploadComponent } from './components/fileupload/fileupload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileuploadComponent
   ],
   imports: [
     BrowserModule,
